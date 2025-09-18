f1d271f6b2b0d937181991250f812cd12e453a8b
leapcoded <leapcoded@users.noreply.github.com>
2025-09-18 12:11:19 +0000

    deploy: d0fae48c7b815be20f15380edcf6213c1a979510

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ import { achievements as allAchievements } from './achievements.js';
 // --- SECTION: FIREBASE & APP INITIALIZATION ---
 let app, auth, db, storage;
 try {
-    const firebaseConfig = %%FIREBASE_CONFIG_PLACEHOLDER%%;
+    const firebaseConfig = {apiKey:AIzaSyB5_SZGECDE170o87N7DlhhVcuL0KLr4Og,authDomain:ink-and-iron-57e6e.firebaseapp.com,projectId:ink-and-iron-57e6e,storageBucket:ink-and-iron-57e6e.firebasestorage.app,messagingSenderId:795969760706,appId:1:795969760706:web:bb82ba49f9880b8b113a5b,measurementId:G-6F3YFPYB0T};
     app = initializeApp(firebaseConfig);
     auth = getAuth(app);
     db = getFirestore(app);
